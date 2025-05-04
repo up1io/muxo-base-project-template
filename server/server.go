def3b0e26d8f4f4b4fec7d8d9e9354fb8bd1dcd2
Jonathan Mausbach <mausbach@up1.io>
2025-05-04 17:34:57 +0200

    fix: mismatch server interface condition

--- server/server.go
+++ server/server.go
@@ -7,6 +7,14 @@ func NewServer() *Server {
 	return &Server{}
 }
 
+func (s *Server) Init() error {
+	return nil
+}
+
 func (s *Server) Run() error {
+	return nil
+}
+
+func (s *Server) Shutdown() []error {
 	return nil
 }
